d293414411a9418f47537a2122643a65800d2721
Dmitry <kronalf@inbox.ru>
2020-09-08 21:59:23 +0300

    First commit

--- src/cards/models.py
+++ src/cards/models.py
@@ -2,25 +2,42 @@ from django.db import models
 
 # Create your models here.
 
-class Person(models.Model):
-    first_name = models.CharField(max_length=200, verbose_name="Имя")
-    last_name = models.CharField(max_length=200, verbose_name="Фамилия")
-    other_name = models.CharField(max_length=200, verbose_name="Отчество")
-
-    class Meta:
-        verbose_name="Пользователь"
-        verbose_name_plural = "Пользователи"
-
 class Department(models.Model):
-    name = models.CharField(max_length=200,verbose_name="Наименование отдела/цеха")
+    name = models.CharField(max_length=200, verbose_name="Наименование отдела/цеха")
 
     class Meta:
-        verbose_name="Отдел"
+        verbose_name = "Отдел"
         verbose_name_plural = "Отделы"
+
+    def __str__(self):
+       return self.name
 
 class Floor(models.Model):
     number = models.IntegerField(verbose_name="Номер этажа")
 
     class Meta:
-        verbose_name="Этаж"
+        verbose_name = "Этаж"
         verbose_name_plural = "Этажи"
+
+    def __str__(self):
+       return self.number
+
+class Person(models.Model):
+    department = models.ForeignKey('Department', on_delete=models.CASCADE, verbose_name="ID Отдел")
+    position = models.CharField(max_length=200, verbose_name="Должность")
+    first_name = models.CharField(max_length=200, verbose_name="Имя")
+    last_name = models.CharField(max_length=200, verbose_name="Фамилия")
+    other_name = models.CharField(max_length=200, verbose_name="Отчество")
+    full_name = models.CharField(max_length=603, verbose_name="Полное имя")
+    login = models.CharField(max_length=200, verbose_name="Имя пользователя")
+    password = models.CharField(max_length=16, verbose_name="Пароль")
+    email = models.EmailField(verbose_name="Почтовый адрес")
+    floor = models.ForeignKey('Floor', on_delete=models.CASCADE, verbose_name="ID Этажа размещения")
+    number_room = models.IntegerField(verbose_name="Номер кабинета")
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.full_name
