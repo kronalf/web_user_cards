17a33c09daf3fa2ac6ace0a09cd3aa7c85c73090
Dmitry <kronalf@inbox.ru>
2020-09-10 23:24:59 +0300

    modernization add_cards

--- src/cards/views.py
+++ src/cards/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-from .models import Person
+from .models import Person, Department
+from .utils import from_cyrillic_to_eng, pswd_generate, login_generate
 
 def get_cards(request):
     qs = Person.objects.all()
     return render(request, 'cards/get_cards.html', {'object_list' : qs})
 
 def add_cards(request):
-    print(request.POST)
-    return render(request, 'cards/add_cards.html')
+    d = Department.objects.all()
+    qs = request.POST
+    first_name = qs['first_name']
+    last_name = qs['last_name']
+    other_name = qs['other_name']
+    full_name = last_name + ' ' + first_name + ' ' + other_name
+    position = qs['position']
+    department = qs['department']
+    number_room = qs['number_room']
+    password = pswd_generate()
+    login = login_generate(last_name, first_name, other_name)
+    print(full_name, login, password)
+    return render(request, 'cards/add_cards.html', {'object_list' : d})
