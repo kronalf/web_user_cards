81e9850a0b03f39467a68a326dfe7f934722d341
Dmitry <kronalf@inbox.ru>
2020-09-13 20:24:23 +0300

    Commit save database

--- src/cards/views.py
+++ src/cards/views.py
@@ -1,6 +1,5 @@
-from django.db import DatabaseError
 from django.shortcuts import render
-from .models import Person, Department, Floor
+from .models import Person, Department
 from .utils import pswd_generate, login_generate
 
 def get_cards(request):
@@ -8,35 +7,23 @@ def get_cards(request):
     return render(request, 'cards/get_cards.html', {'object_list' : qs})
 
 def add_cards(request):
-    qs = request.POST
-    person = {}
-    password = pswd_generate()
     d = Department.objects.all()
 
     if request.GET:
         pass
     if request.POST:
-        department = Department.objects.filter(name=qs['department']).first()
-        login = login_generate(qs['last_name'].strip(),
-                               qs['first_name'].strip(),
-                               qs['other_name'].strip())
+        qs = request.POST
+        first_name = qs['first_name']
+        last_name = qs['last_name']
+        other_name = qs['other_name']
+        full_name = last_name + ' ' + first_name + ' ' + other_name
+        position = qs['position']
+        department = qs['department']
+        number_room = qs['number_room']
+        password = pswd_generate()
+        login = login_generate(last_name, first_name, other_name)
         email = login + '@minudo.ru'
-        # n = qs['number_room'][0]
-        floor = Floor.objects.filter(number=qs['number_room'][0]).first()
-        person = {'first_name' : qs['first_name'].strip(),
-                  'last_name' : qs['last_name'].strip(),
-                  'other_name' : qs['other_name'].strip(),
-                  'full_name' : qs['last_name'] + ' ' + qs['first_name'] + ' ' + qs['other_name'],
-                  'position' : qs['position'].strip(),
-                  'number_room' : qs['number_room'].strip(),
-                  'password' : password,
-                  'login' : login,
-                  'email' : email}
-        p = Person(**person, department=department, floor=floor)
-        try:
-            p.save()
-        except DatabaseError:
-            pass
-        print(person)
+        floor = number_room[0]
+        print(full_name, login, email, password, floor)
 
     return render(request, 'cards/add_cards.html', {'object_list' : d})
