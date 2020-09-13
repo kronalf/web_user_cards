0afe1204673b50477233436838476a9acb36644b
Dmitry <kronalf@inbox.ru>
2020-09-13 21:44:24 +0300

    Switch add_cards to get_cards

--- src/cards/views.py
+++ src/cards/views.py
@@ -3,14 +3,15 @@ from django.shortcuts import render
 from .models import Person, Department, Floor
 from .utils import pswd_generate, login_generate
 
+
 def get_cards(request):
     qs = Person.objects.all()
-    return render(request, 'cards/get_cards.html', {'object_list' : qs})
+    return render(request, 'cards/get_cards.html', {'object_list': qs})
+
 
 def add_cards(request):
     qs = request.POST
     d = Department.objects.all()
-    person = {}
 
     if request.GET:
         pass
@@ -24,23 +25,23 @@ def add_cards(request):
         department = Department.objects.filter(name=qs['department']).first()
         floor = Floor.objects.filter(number=qs['number_room'][0]).first()
         password = pswd_generate()
-        person = {'first_name':first_name,
-                  'last_name':last_name,
-                  'other_name':other_name,
-                  'full_name':full_name,
-                  'position':qs['position'].strip(),
-                  'department':department,
-                  'number_room':qs['number_room'],
-                  'password':password,
-                  'login':login,
-                  'email':email,
-                  'floor':floor}
+        person = {'first_name': first_name,
+                  'last_name': last_name,
+                  'other_name': other_name,
+                  'full_name': full_name,
+                  'position': qs['position'].strip(),
+                  'department': department,
+                  'number_room': qs['number_room'],
+                  'password': password,
+                  'login': login,
+                  'email': email,
+                  'floor': floor}
         p = Person(**person)
         try:
             p.save()
             return render(request, 'cards/get_cards.html',
-                   {'object_list' : Person.objects.filter(login=login)})
+                          {'object_list': Person.objects.filter(login=login)})
         except DatabaseError:
             pass
 
-    return render(request, 'cards/add_cards.html', {'object_list' : d})
+    return render(request, 'cards/add_cards.html', {'object_list': d})
